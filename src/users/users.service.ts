1e603549c5acb64e08b5cb96e062896f47e5e5d4
NatureSL <varistrue@gmail.com>
2023-10-21 22:45:18 +0600

    feat: implementation of the block list module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { AccountService } from 'src/account/account.service';
+import { BlockListService } from 'src/block-list/block-list.service';
 import { DbService } from 'src/db/db.service';
 
 @Injectable()
@@ -8,7 +9,11 @@ export class UsersService {
    * Подключаем DB через DI
    * @param db
    */
-  constructor(private db: DbService, private accountService: AccountService) {}
+  constructor(
+    private db: DbService,
+    private accountService: AccountService,
+    private blockList: BlockListService,
+  ) {}
 
   /**
    * Метод для логина и регистрации
@@ -29,7 +34,8 @@ export class UsersService {
   async create(email: string, hash: string, salt: string) {
     const user = await this.db.user.create({ data: { email, hash, salt } });
     await this.accountService.create(user.id);
+    await this.blockList.create(user.id);
     
-    return user
+    return user;
   }
 }
